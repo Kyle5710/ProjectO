39b63388a4755686069e0c2fc63120ee48e487f0
Kyle5710 <7006960@spsd.sk.ca>
2024-11-27 14:54:29 -0600

    fix
    .

--- sketch.js
+++ sketch.js
@@ -1,14 +1,42 @@
-let titleScreen;
+//title variables
+let titleScreen, titleButton;
 
-function preload(){
-	titleScreen = loadImage("favicon.png");
+//state variables
+let currentBackground, currentScene;
+
+function preload() {
+	//load font file
+	font = loadFont("assets/font/font.ttf");
+
+	//load necessary title assets
+	titleScreen = loadImage("assets/title/titleBack.png");
+	titleButton = loadImage("assets/title/titleButton.png");
 }
 
 function setup() {
 	new Canvas(640, 360);
 	displayMode("centered", "pixelated");
+
+	//set font
+	textFont(font);
+
+	//update current scene
+	currentScene = "Title";
+	determineBackground();
 }
 
 function draw() {
-	background(titleScreen);
+	//set background
+	background(currentBackground);
+	determineBackground();
+}
+
+function determineBackground() {
+	//have a state var that tells us what scene we are in
+	//and use it to determine what background should be
+	//displayed on screen
+
+	if (currentScene === "Title") {
+		currentBackground = titleScreen;
+	}
 }
